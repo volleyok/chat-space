9c56b9e862bc206b4d28f082377c78ef501138e7
volleyok <56710628+volleyok@users.noreply.github.com>
2019-10-26 14:46:12 +0900

    incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?) and id != ?', "#{params[:keyword]}%" ,current_user)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
