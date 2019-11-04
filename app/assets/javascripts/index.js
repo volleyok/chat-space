94e606f1a35923a04784015e2daadbebd9fbc982
volleyok <56710628+volleyok@users.noreply.github.com>
2019-11-04 21:10:42 +0900

    変更

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function() {
   　$(function() {
     function addUser(user) {
       let html = `
-        <div class="chat-group-user clearfix">
+        <div class="chat-group-user clearfix" id='chat-group-user-${user.id}'>
           <p class="chat-group-user__name">${user.name}</p>
           <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
         </div>`;
